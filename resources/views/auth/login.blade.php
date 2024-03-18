06682893a4658928a0eab2dfb594d7c33a1a691e
monoj kumar <monoj.womenindigital@gmail.com>
2024-03-18 11:15:59 +0600

    login , reset password update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -88,53 +88,64 @@
 @extends('layouts.user.master')
 
 @section('content')
-   <div class="content mb-5">
-      <div class="content-element">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-md-6">
-              <h2 class="login-header">Login to your account</h2>
-              <div class="form-background">
-                <form class="log-in-form">
-                  <div class="mb-3">
-                    <label for="name" class="form-label">Username/ID:</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="name"
-                      aria-describedby="emailHelp"
-                    />
-                  </div>
+    <div class="content mb-5">
+        <div class="content-element">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-6">
+                        <h2 class="login-header">Welcome back! Log in to your account.</h2>
+                        <div class="form-background">
+                            <form class="log-in-form" method="POST" action="{{ route('login') }}">
+                                @csrf
 
-                  <div class="mb-3">
-                    <label for="password" class="form-label">Password:</label>
-                    <div class="password-container">
-                      <input
-                        type="password"
-                        class="form-control"
-                        id="password"
-                        aria-describedby="emailHelp"
-                      />
-                      <span
-                        class="toggle-password"
-                        onclick="togglePasswordVisibility()"
-                      ></span>
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email address:</label>
+                                    <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                        name="email" id="email" placeholder="Email"value="{{ old('email') }}"
+                                        autocomplete="email" autofocus>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Password:</label>
+                                    <div class="password-container">
+                                        <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                            name="password" id="password" required autocomplete="current-password"
+                                            placeholder="Password">
+                                        <span class="toggle-password" onclick="togglePasswordVisibility()"></span>
+                                    </div>
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-check mb-3">
+                                    <input type="checkbox" class="form-check-input"name="remember"
+                                        id="authCheck"{{ old('remember') ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="authCheck">
+                                        Remember me
+                                    </label>
+                                </div>
+                               
+                                  @if (Route::has('password.request'))
+                                                <a href="{{ route('password.request') }}"
+                                                    class="forget-pass">Forget password</a>
+                                            @endif
+                                <button type="button" class="custom-btn w-100">LOGIN</button>
+                                <p class="login-footer">Are you a new user?</p>
+                                <a href="{{route('register')}}" class="custom-btn mt-2 text-center w-100">
+                                    SIGN UP
+                                </a>
+                            </form>
+                        </div>
                     </div>
-                  </div>
-                  <p class="forget-pass">
-                    <a href="#">Forgot/Unlock my password?</a>
-                  </p>
-                  <button type="button" class="custom-btn w-100">LOGIN</button>
-                  <p class="login-footer">Are you a new user?</p>
-                  <a href="/signup.html" class="custom-btn mt-2 text-center w-100">
-                   SIGN UP
-                  </a>
-                </form>
-              </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -62,4 +62,73 @@
         </div>
     </div>
 </div>
+@endsection --}}
+
+
+@extends('layouts.user.master')
+
+@section('content')
+    <div class="content mb-5">
+        <div class="content-element">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-6">
+                        <h2 class="login-header">Reset Password</h2>
+                        <div class="form-background">
+                            <form class="log-in-form" method="POST" action="{{ route('password.update') }}">
+                                @csrf
+                                <input type="hidden" name="token" value="{{ $token }}">
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email address:</label>
+                                    <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                        name="email" id="email" placeholder="Email"value="{{ old('email') }}"
+                                        autocomplete="email" autofocus>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Password:</label>
+                                    <div class="password-container">
+                                        <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                            name="password" id="password" required autocomplete="current-password"
+                                            placeholder="Password">
+                                        <span class="toggle-password" onclick="togglePasswordVisibility()"></span>
+                                    </div>
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="mb-3">
+                                    <label for="password-confirm" class="form-label">Confirm Password:</label>
+                                    <div class="password-container">
+                                        <div class="col-md-6">
+                                            <input id="password-confirm" type="password" class="form-control"
+                                                name="password_confirmation" required autocomplete="new-password">
+                                        </div>
+                                        <span class="toggle-password" onclick="togglePasswordVisibility()"></span>
+                                    </div>
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+
+
+                                <button type="button" class="custom-btn w-100">Reset Password</button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
