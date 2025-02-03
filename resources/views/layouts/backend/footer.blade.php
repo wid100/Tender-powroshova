678253fe4cb1c3caba85012ebaa5dceb9ba85f6c
wid100 <helal.womenindigital@gmail.com>
2025-02-03 16:17:26 +0600

    update

--- resources/views/layouts/backend/footer.blade.php
+++ resources/views/layouts/backend/footer.blade.php
@@ -1,4 +1,4 @@
 <footer
     class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
-    <p class="text-muted mb-1 mb-md-0">Copyright © 2022 <a href="https://www.womenindigital.net" target="_blank">Women In Digital</a>.</p>
+    <p class="text-muted mb-1 mb-md-0">Copyright © 2025 <a href="https://www.womenindigital.net" target="_blank">Women In Digital</a>.</p>
 </footer>
